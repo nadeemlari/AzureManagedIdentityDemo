4811579631201b32a0b37080c60065cf8a476554
Nadeem Lari <nadeemlari@gmail.com>
2021-11-13 10:17:02 +0530

    Add project files.

---
+++ AzureManagedIdentityDemo/Controllers/BlobController.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AzureManagedIdentityDemo.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class BlobController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string stgConnection = "https://mnl.blob.core.windows.net/container1";
+            BlobContainerClient blobContainerClient = new BlobContainerClient(new Uri(stgConnection), new DefaultAzureCredential());
+            BlobClient blobClient = blobContainerClient.GetBlobClient("MyPic.jpg");
+            var content = blobClient.DownloadContent().Value.Content.ToArray();
+            return File(content, "img/png", "a.jpg");
+        }
+    }
+}
---
+++ AzureManagedIdentityDemo/Startup.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Blobs;
+using Azure.Core.Extensions;
+
+namespace AzureManagedIdentityDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddAzureClients(builder =>
+            {
+                builder.AddBlobServiceClient(Configuration["str:blob"], preferMsi: true);
+                builder.AddQueueServiceClient(Configuration["str:queue"], preferMsi: true);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+    internal static class StartupExtensions
+    {
+        public static IAzureClientBuilder<BlobServiceClient, BlobClientOptions> AddBlobServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
+        {
+            if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
+            {
+                return builder.AddBlobServiceClient(serviceUri);
+            }
+            else
+            {
+                return builder.AddBlobServiceClient(serviceUriOrConnectionString);
+            }
+        }
+        public static IAzureClientBuilder<QueueServiceClient, QueueClientOptions> AddQueueServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
+        {
+            if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
+            {
+                return builder.AddQueueServiceClient(serviceUri);
+            }
+            else
+            {
+                return builder.AddQueueServiceClient(serviceUriOrConnectionString);
+            }
+        }
+    }
+}
